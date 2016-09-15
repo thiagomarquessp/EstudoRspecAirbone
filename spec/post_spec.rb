f590d4608c84f6ff69fb36b06d34720660acc123
Thiago Biro Biro <thiagomarquessp@gmail.com>
2016-09-15 00:04:30 -0300

    First Commit.~

---
+++ spec/post_spec.rb
@@ -0,0 +1,8 @@
+require 'airborne'
+
+describe 'Get Marotis' do
+  it 'Get nosso de cada dia' do
+    get 'http://jsonplaceholder.typicode.com/posts/1' #json api that returns { "name" : "John Doe" }
+    expect_json_types(title: 'Blablablabla')
+  end
+end
