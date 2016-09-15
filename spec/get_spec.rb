f6be6eec8c10e1c9e5317a92fb544268abf1455f
Biro Biro <thiagomarquessp@gmail.com>
2016-09-15 17:31:10 -0300

    Get scenario.

--- spec/get_spec.rb
+++ spec/get_spec.rb
@@ -3,6 +3,6 @@ require 'airborne'
 describe 'Get Marotis' do
   it 'Get nosso de cada dia' do
     get 'http://jsonplaceholder.typicode.com/posts/1' #json api that returns { "name" : "John Doe" }
-    expect_json_types(title: 'Blablablabla')
+    expect_json_types(title: :string)
   end
 end
